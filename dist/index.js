ebf30c1efa2760ea95ef11f6da04ae6f8f7703d6
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2025-12-01 17:00:12 +0600

    recape-typescript initialized

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+let age = 23;
+let num = 123;
+console.log(age);
+console.log(num);
+const surName = 'rifat';
+console.log(surName);
+const boolean = true;
+console.log(boolean);
+const numbers = [1, 2, 3, 4, 5];
+const names = ['rifat', 'abu', 'sayed'];
+names[2].concat('ur name');
+console.log(names);
+const basket = ['banana', 24];
+let mySize = 3;
+console.log(mySize);
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+let age:number = 23;
+let num: number= 123;
+console.log(age);
+console.log(num);
+ 
+
+const surName: string = 'rifat'
+console.log(surName);
+
+const boolean: boolean = true
+console.log(boolean);
+
+
+//array
+
+const numbers : number[] = [1,2,3,4,5]
+
+const names : string [] = ['rifat', 'abu', 'sayed']
+
+names[2].concat('ur name')
+
+console.log(names);
+
+// tuple
+// tuple only usefull when we use only two values
+
+const basket : [string, number] = ['banana', 24]
+
+// enum         
+
+  const enum size { Small = 1, Medium , Large}
+
+  let mySize = size.Large
+  console.log(mySize);
+   
+
