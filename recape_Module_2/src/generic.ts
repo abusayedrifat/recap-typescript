52ca7e7ed1fe21437fd22c5b10f2aa642f164ce3
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2025-12-27 22:40:58 +0600

    update

--- recape_Module_2/src/generic.ts
+++ recape_Module_2/src/generic.ts
@@ -2,6 +2,19 @@
 
 // generic type
 
+type Generic<T> = Array<T>
+
+// const numbers : number[] = [1,2,3]
+const numbers : Generic<number> = [1,2,3]
+
+// const names : string[] = ['abu', 'sayed', 'rifat']
+const names : Generic<string> = ['abu', 'sayed', 'rifat']
+
+// const booleans : boolean[] = [true, false, true, true]
+const booleans : Generic<boolean> = [true, false, true, true]
+
+
+//Object
 
 
 
