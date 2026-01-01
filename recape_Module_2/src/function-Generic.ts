51dddf8c8f385ec4d77e004145acde81cac69376
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-01 22:17:15 +0600

    update

--- recape_Module_2/src/function-Generic.ts
+++ recape_Module_2/src/function-Generic.ts
@@ -1,7 +1,25 @@
 {
 
 
-    const 
+    const createArray = (param: string): string[] =>{
+        return [param]
+    }
+
+    const createArrayWithGeneric = <T> (param:T):T[]=>{
+        return [param]
+    }
+
+    const res = createArray('bangladesh')
+
+    type Obj = {
+        id:number,
+        name:string
+    }
+    const res2 = createArrayWithGeneric<Obj>({id:23, name:'rifat'})
+
+
+
+
 
 
 
