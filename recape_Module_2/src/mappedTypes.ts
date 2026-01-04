d5d6c2c2b447dc4445d2eb859e6044c030829163
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-04 08:32:22 +0600

    mapped types

--- recape_Module_2/src/mappedTypes.ts
+++ recape_Module_2/src/mappedTypes.ts
@@ -1,13 +1,33 @@
 {
 
 
-    //mapped types
+   
 
     const arrOfNummbes: number[] = [1,2,3,4]
 
     const arrOfString:string[] = arrOfNummbes.map((number)=> number.toString())
     console.log(arrOfString);
     
+ //mapped types
+ //(means we can change the type of every property in an object, array)
+
+
+ type AreaNumber = {
+    width:number;
+    height:number
+ }
+
+//  type AreaNumber = {
+//     width:string;
+//     height:string
+//  }
+
+//note: The keyof operator in TypeScript creates a union type of all the keys (property names) of an object type. 
+
+type AreaString = {
+    [key in keyof AreaNumber] : string
+}
+ 
 
 
 
