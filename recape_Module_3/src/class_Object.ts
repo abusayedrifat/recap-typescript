63b41863370c4f519e66f91b44f771bedd8b34a9
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-05 08:07:03 +0600

    class object

--- recape_Module_3/src/class_Object.ts
+++ recape_Module_3/src/class_Object.ts
@@ -1,5 +1,19 @@
 {
 
+    //modifiers in class -> public, private, readonly, protected
 
-    const name: string = 'rifat'
+    class Animal {
+       
+    constructor(public name:string, public species:string , public sound:string ){}
+    
+
+     makeSound(){
+        console.log(`${this.name} sounds like ${this.sound}`);
+        
+     }
+    }
+
+
+    const cat = new Animal ("paki cat","tiger" , "mew")
+    cat.makeSound()
 }
