6dcdb07b9384270092c473ab66731f0a61c3e357
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-06 10:30:11 +0600

    update

--- recape_Module_3/src/typeGaurd.ts
+++ recape_Module_3/src/typeGaurd.ts
@@ -1,11 +1,50 @@
 {
 
     //type gaurd / narrowing
+
+
+
     
 
 
 
 
+
+
+
+
+
+
+type NormalUser = {
+    name:string,
+
+}
+type AdminUser = {
+    name:string,
+    role: "admin"
+}
+
+const getUser = (user: NormalUser | AdminUser) =>{
+    if ("role" in user) {
+        console.log(`my name is ${user.name} and i am ${user.role}`);
+        
+    }else{
+        console.log(`my name is ${user.name} and i am normal user`);
+        
+    }
+}
+
+const normalUser: NormalUser = {
+    name:'public'
+}
+
+const adminUser: AdminUser = {
+    name:'mr.X',
+    role:'admin'
+}
+
+
+console.log(getUser(adminUser));
 
 
 
