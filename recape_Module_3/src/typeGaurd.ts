6ccb29f7610726770000cedf595e5d13f5d32df9
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-07 00:02:55 +0600

    upadate

--- recape_Module_3/src/typeGaurd.ts
+++ recape_Module_3/src/typeGaurd.ts
@@ -1,19 +1,32 @@
 {
 
     //type gaurd / narrowing
+    //=================================
 
 
 
-    
+    //"typeof" narrowing
+
+type AlphaNumeric = number | string
+
+const sum = (param1:AlphaNumeric, param2:AlphaNumeric) :AlphaNumeric =>{
+    if ( typeof param1 === "number" && typeof param2 === "number") {
+        return param1+param2
+    }
+    else{
+        return param1.toString()+param2.toString()
+    }
+}
+
+
+
+console.log(sum('2','3'));
 
 
 
 
 
-
-
-
-
+// "in" gaurd narowing
 
 type NormalUser = {
     name:string,
