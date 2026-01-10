954985d7adcedc0d0449242bf9866d0b0f681286
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-10 22:49:26 +0600

    update

---
+++ recape_Module_3/src/accessModifiers.ts
@@ -0,0 +1,46 @@
+{
+
+    // access modifiers
+
+
+    class BankAccount{
+        name : string;
+        readonly id : number;
+        private balance: number;
+
+        constructor(id:number, name:string, balance:number){
+            this.id = id;
+            this.name = name;
+            this.balance = balance
+        }
+
+        addDeposite(newBalance : number){
+            this.balance = this.balance + newBalance;
+            console.log(this.balance);
+            
+        }
+    }
+
+    
+    const account = new BankAccount(123, 'abu sayed', 500)
+    // account.addDeposite(10)
+    
+    // console.log(account.addDeposite(20))
+    
+    console.log(account.balance);
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
