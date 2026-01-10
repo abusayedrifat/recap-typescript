827b95c45d06652c543cc3a1088ff1c387ef5e1b
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-10 23:03:27 +0600

    getter and setter

---
+++ recape_Module_3/src/Gettter_Setter.ts
@@ -0,0 +1,26 @@
+{
+// getter & setter
+//Getters and setters in TypeScript allow you to control access to class properties with custom logic. They look like properties when used but execute functions behind the scenes.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- recape_Module_3/src/accessModifiers.ts
+++ recape_Module_3/src/accessModifiers.ts
@@ -25,9 +25,9 @@
     const account = new BankAccount(123, 'abu sayed', 500)
     // account.addDeposite(10)
     
-    // console.log(account.addDeposite(20))
+    console.log(account.addDeposite(20))
     
-    console.log(account.balance);
+    // console.log(account.balance);
    
     
 
