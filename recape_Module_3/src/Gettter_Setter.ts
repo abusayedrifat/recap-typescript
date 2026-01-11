d52f521b3d684d128c257f8d53ec6b66eb6fadcd
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-11 22:12:50 +0600

    update

--- recape_Module_3/src/Gettter_Setter.ts
+++ recape_Module_3/src/Gettter_Setter.ts
@@ -3,8 +3,32 @@
 //Getters and setters in TypeScript allow you to control access to class properties with custom logic. They look like properties when used but execute functions behind the scenes.
 
 
+class Temperature {
+    tempInCelcius : number;
+    // tempInFerhenhiet: number;
+
+    constructor(tempInCelcius:number){
+        this.tempInCelcius = tempInCelcius;
+        // this.tempInFerhenhiet = tempInFerhenhiet
+    }
+
+    get celciusToFer():number{
+        if (this.tempInCelcius < -547) {
+            throw new Error('Temperature below absolute zero!')
+            
+        }
+        
+        return (this.tempInCelcius*9/5)+32;
+    }
 
 
+}
+
+
+const temp = new Temperature(-73)
+
+
+console.log(temp.celciusToFer);
 
 
 
