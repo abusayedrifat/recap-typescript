1a230d868240c778e3f0c65dfe6511addd6e9dac
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2026-01-16 00:19:40 +0600

    update

--- recape_Module_3/src/polymorphism.ts
+++ recape_Module_3/src/polymorphism.ts
@@ -2,11 +2,51 @@
 //polymorphism
 
 
+class Person{
+    getSleep(){
+        console.log('i am sleeping 8 hours');
+        
+    }
+}
+
+
+class Student extends Person{
+    getSleep(){
+        console.log('i am sleeping 6 hours');
+        
+    }
+}
+
+class Developer extends Person{
+    getSleep(): void {
+        console.log(' i am sleeping 5 hours');
+        
+    }
+}
+
+
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new Developer();
+
+
+const getSleepingHours = (param: Person) =>{
+    param.getSleep()
+}
+
+getSleepingHours(person1)
+getSleepingHours(person2)
+getSleepingHours(person3)
 
 
 
+class Shape {
+    getArea():number{
+        return 0
+    }
+}
 
-
+class Circle extends 
 
 
 
